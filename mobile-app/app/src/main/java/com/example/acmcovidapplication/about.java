fa273a656a609634f6dc48401f6a496e35c9ab02
Shehana Iqbal <iqbalshehana@gmail.com>
2020-05-10 00:02:28 +0530

    About UI modification

--- mobile-app/app/src/main/java/com/example/acmcovidapplication/about.java
+++ mobile-app/app/src/main/java/com/example/acmcovidapplication/about.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class about extends AppCompatActivity {
 
@@ -12,6 +14,13 @@ public class about extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Button btn=findViewById(R.id.back_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
