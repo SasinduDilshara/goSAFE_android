0eb50cc8b3711bca66f3156edc8d3feef54ff75c
Zameel <Zameel@gmail.com>
2020-05-10 09:05:48 +0530

    Runtime permission included

--- mobile-app/app/src/main/java/com/example/acmcovidapplication/MainActivity.java
+++ mobile-app/app/src/main/java/com/example/acmcovidapplication/MainActivity.java
@@ -1,21 +1,64 @@
 package com.example.acmcovidapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.example.acmcovidapplication.services.Util;
+
+import static com.example.acmcovidapplication.services.Util.PERMISSION_REQUEST_BACKGROUND_LOCATION;
+import static com.example.acmcovidapplication.services.Util.PERMISSION_REQUEST_COARSE_LOCATION;
+import static com.example.acmcovidapplication.services.Util.PERMISSION_REQUEST_FINE_LOCATION;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = ".acmcovidapplication";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Util.requestPermission(this);
     }
 
     public void goToLogin(View view) {
         Intent intent = new Intent(MainActivity.this, login.class);
         startActivity(intent);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
---
+++ mobile-app/app/src/main/java/com/example/acmcovidapplication/services/Util.java
@@ -0,0 +1,38 @@
+package com.example.acmcovidapplication.services;
+
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+public class Util {
+
+    public static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
+    public static final int PERMISSION_REQUEST_BACKGROUND_LOCATION = 2;
+    public static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
+
+
+    public static void requestPermission(final Activity context) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // Android M Permission check
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("This app needs location access");
+                builder.setMessage("Please grant location access so this app can detect beacons.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+
+                    public void onDismiss(DialogInterface dialog) {
+                        context.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_REQUEST_COARSE_LOCATION);
+                    }
+                });
+                builder.show();
+            }
+        }
+    }
+}
