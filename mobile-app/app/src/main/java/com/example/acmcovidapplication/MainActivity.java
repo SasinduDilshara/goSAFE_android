91843f6d5caac30461efc7f0733a42edc5ecfbf7
Shehana Iqbal <iqbalshehana@gmail.com>
2020-05-16 21:40:22 +0530

    splash screen added

--- mobile-app/app/src/main/java/com/example/acmcovidapplication/MainActivity.java
+++ mobile-app/app/src/main/java/com/example/acmcovidapplication/MainActivity.java
@@ -1,21 +1,40 @@
 package com.example.acmcovidapplication;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.os.Bundle;
-import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-    public void goToLogin(View view) {
-        Intent intent = new Intent(MainActivity.this, login.class);
-        startActivity(intent);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
+        setContentView(R.layout.activity_main);
+        //this will bind your MainActivity.class file with activity_main.
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(MainActivity.this,
+                        home.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 }
+
