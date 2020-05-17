a2f76078059160fae68e8f7a7c8a2a38cfa5b789
Shehana Iqbal <iqbalshehana@gmail.com>
2020-05-17 23:04:40 +0530

    UI finalized

--- mobile-app/app/src/main/java/com/example/acmcovidapplication/otp_entering.java
+++ mobile-app/app/src/main/java/com/example/acmcovidapplication/otp_entering.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class otp_entering extends AppCompatActivity {
 
@@ -12,10 +13,20 @@ public class otp_entering extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp_entering);
+        Button btn=findViewById(R.id.back_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     public void appPermissions(View view) {
         Intent intent = new Intent(otp_entering.this, appPermission.class);
         startActivity(intent);
+    }
+    public void resendCode(View view){
+
     }
 }
