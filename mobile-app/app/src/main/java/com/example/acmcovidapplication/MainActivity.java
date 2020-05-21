3fbd73d8384a4063ddac67b1035bf9a32bce3622
Shehana Iqbal <iqbalshehana@gmail.com>
2020-05-21 22:04:27 +0530

    splash-screen-anim

--- mobile-app/app/src/main/java/com/example/acmcovidapplication/MainActivity.java
+++ mobile-app/app/src/main/java/com/example/acmcovidapplication/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
 import com.example.acmcovidapplication.db.DatabaseHelper;
@@ -16,19 +19,20 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
 
-    private static int SPLASH_SCREEN_TIME_OUT = 2000;
+    private static int SPLASH_SCREEN_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //This method is used so that your splash activity
         //can cover the entire screen.
 
         setContentView(R.layout.activity_main);
-
+        ImageView img=findViewById(R.id.imageView);
+        Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        img.startAnimation(animFadeIn);
         //this will bind your MainActivity.class file with activity_main.
 
         new Handler().postDelayed(new Runnable() {
